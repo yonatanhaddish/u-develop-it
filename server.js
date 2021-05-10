1130c38ba09c7082df444b1fceb24f279d52b064
Your Name <you@example.com>
2021-05-10 13:24:17 -0400

    connect to database

---
+++ server.js
@@ -0,0 +1,29 @@
+const mysql= require('mysql2');
+const express= require('express');
+
+const PORT= process.env.PORT || 3001;
+const app= express();
+
+// Express middleware
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+// Connect to database
+const db= mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '071780yonihiinQ',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+// Default response for any other request (Not Found)
+app.get((req, res) => {
+    res.status(404).end();
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
