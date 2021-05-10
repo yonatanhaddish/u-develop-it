bcd5c024386ce1afed766b8088628fcb8b868862
Your Name <you@example.com>
2021-05-10 14:24:49 -0400

    database query

--- server.js
+++ server.js
@@ -19,6 +19,10 @@ const db= mysql.createConnection(
     console.log('Connected to the election database.')
 );
 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
+
 // Default response for any other request (Not Found)
 app.get((req, res) => {
     res.status(404).end();
